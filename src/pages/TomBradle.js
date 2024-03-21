e523c78f2b83eb10edea010fcf387a6d1a706d88
VA_24 <24agnihotriv@smtexas.org>
2024-03-21 17:02:22 -0500

    added home

---
+++ src/pages/TomBradle.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect} from 'react';
+import '../App.css';
+
+function TomBradle(){
+    return(
+        <body class='bg-white min-h-screen m-0'>
+               test     
+        </body>
+    )
+}
+export default TomBradle;
